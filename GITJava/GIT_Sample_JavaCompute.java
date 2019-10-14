879880d157390203e934d548d993580f39aac3f1
prathapreddy <prathapreddy.eidiko@gmail.com>
2019-10-14 14:35:49 +0530

    Updated GIT Application V1

--- GITJava/GIT_Sample_JavaCompute.java
+++ GITJava/GIT_Sample_JavaCompute.java
@@ -1,9 +1,11 @@
 import com.ibm.broker.javacompute.MbJavaComputeNode;
+import com.ibm.broker.plugin.MbElement;
 import com.ibm.broker.plugin.MbException;
 import com.ibm.broker.plugin.MbMessage;
 import com.ibm.broker.plugin.MbMessageAssembly;
 import com.ibm.broker.plugin.MbOutputTerminal;
 import com.ibm.broker.plugin.MbUserException;
+import com.ibm.broker.plugin.MbXMLNSC;
 
 
 public class GIT_Sample_JavaCompute extends MbJavaComputeNode {
@@ -20,7 +22,19 @@ public class GIT_Sample_JavaCompute extends MbJavaComputeNode {
 			outAssembly = new MbMessageAssembly(inAssembly, outMessage);
 			// ----------------------------------------------------------
 			// Add user code below
-
+			MbElement firstRoot = inMessage.getRootElement().getFirstElementByPath("XMLNSC/DETAILS/FIRSTNAME");
+			String str1 = firstRoot.getValueAsString();
+			MbElement secondRoot = inMessage.getRootElement().getFirstElementByPath("XMLNSC/DETAILS/LASTNAME");
+			String str2 = secondRoot.getValueAsString();
+			MbElement thirdRoot = inMessage.getRootElement().getFirstElementByPath("XMLNSC/DETAILS/MOBILE");
+			String str3 = thirdRoot.getValueAsString();
+						
+			MbElement outRoot = outMessage.getRootElement();
+			MbElement outParser1 = outRoot.createElementAsLastChild(MbXMLNSC.PARSER_NAME);
+			MbElement outMsg = outParser1.createElementAsLastChild(MbXMLNSC.FOLDER, "ACCOUNTINFO", null);
+			outMsg.createElementAsLastChild(MbXMLNSC.FIELD, "NAME", str1);
+			outMsg.createElementAsLastChild(MbXMLNSC.FIELD, "SURNAME", str2);
+			outMsg.createElementAsLastChild(MbXMLNSC.FIELD, "PHONE", str3);
 			// End of user code
 			// ----------------------------------------------------------
 		} catch (MbException e) {
