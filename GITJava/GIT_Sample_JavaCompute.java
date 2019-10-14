67217442d5b2dcfe11ef30802d38e7fbb24aef1e
prathapreddymerugu <prathapreddy.eidiko@gmail.com>
2019-10-14 14:37:10 +0530

    Update GIT_Sample_JavaCompute.java

--- GITJava/GIT_Sample_JavaCompute.java
+++ GITJava/GIT_Sample_JavaCompute.java
@@ -22,19 +22,7 @@ public class GIT_Sample_JavaCompute extends MbJavaComputeNode {
 			outAssembly = new MbMessageAssembly(inAssembly, outMessage);
 			// ----------------------------------------------------------
 			// Add user code below
-			MbElement firstRoot = inMessage.getRootElement().getFirstElementByPath("XMLNSC/DETAILS/FIRSTNAME");
-			String str1 = firstRoot.getValueAsString();
-			MbElement secondRoot = inMessage.getRootElement().getFirstElementByPath("XMLNSC/DETAILS/LASTNAME");
-			String str2 = secondRoot.getValueAsString();
-			MbElement thirdRoot = inMessage.getRootElement().getFirstElementByPath("XMLNSC/DETAILS/MOBILE");
-			String str3 = thirdRoot.getValueAsString();
-						
-			MbElement outRoot = outMessage.getRootElement();
-			MbElement outParser1 = outRoot.createElementAsLastChild(MbXMLNSC.PARSER_NAME);
-			MbElement outMsg = outParser1.createElementAsLastChild(MbXMLNSC.FOLDER, "ACCOUNTINFO", null);
-			outMsg.createElementAsLastChild(MbXMLNSC.FIELD, "NAME", str1);
-			outMsg.createElementAsLastChild(MbXMLNSC.FIELD, "SURNAME", str2);
-			outMsg.createElementAsLastChild(MbXMLNSC.FIELD, "PHONE", str3);
+			
 			// End of user code
 			// ----------------------------------------------------------
 		} catch (MbException e) {
